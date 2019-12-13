6082be38de98a22b6e9eca9b90c43dbb02e9b8ca
lee-aaron <lee.aaron.68@gmail.com>
2019-12-13 00:35:31 -0800

    Added weights from another model but will create own training set

---
+++ client/src/components/Upload.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Upload extends React.Component {
+
+    handleForm(e) {
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <form>
+
+            </form>
+        );
+    }
+}
+
+export default Upload;
--- model/README.md
+++ model/README.md
@@ -1,2 +1,4 @@
 # Model
-This contains the training for the data on the deep learning model for upscaling
+This contains the training for the data on the deep learning model for upscaling.
+
+https://github.com/titu1994/Image-Super-Resolution
